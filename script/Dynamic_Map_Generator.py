718f496ad2c376adf374373e6ec3b41e95043e25
jeffersonkim97 <jeffersonkim97@gmail.com>
2023-07-19 16:12:51 -0400

    update

---
+++ script/Dynamic_Map_Generator.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random as rn
+import shapely
+from shapely import geometry
+
+# 3D Plot
+from mpl_toolkits import mplot3d
+
+def map_size():
+    # Generate 
+
+
+#%%
+if __name__ == "__main__":
+    import numpy as np
+    from Camera import Camera
+    from Dynamic import DynamicMap
+
+    # Map
+    map_in = {}
+
+    # Static_map
+    map_in['st'] = {}
+    map_in['st']['size'] = np.array([])
